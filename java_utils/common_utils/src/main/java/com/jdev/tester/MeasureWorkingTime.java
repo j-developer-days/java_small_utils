fca310c78e7bf94d75796eac30aef229ec11204b
j_dev <j.developer.days@gmail.com>
2023-01-20 15:05:26 +0200

    -measure working time

--- java_utils/common_utils/src/main/java/com/jdev/tester/MeasureWorkingTime.java
+++ java_utils/common_utils/src/main/java/com/jdev/tester/MeasureWorkingTime.java
@@ -2,27 +2,55 @@ package com.jdev.tester;
 
 import lombok.NoArgsConstructor;
 
+import java.time.Duration;
+import java.time.LocalTime;
+
 @NoArgsConstructor
 public class MeasureWorkingTime {
 
-    private long beginTime;
-    private long endTime;
+    private long beginNanoSeconds;
+    private long endNanoSeconds;
 
-    public long calculate() {
-        return this.endTime - this.beginTime;
+    private long beginMillisSeconds;
+    private long endMillisSeconds;
+
+    private LocalTime beginLocalTime;
+    private LocalTime endLocalTime;
+
+    public void beginNanoSeconds() {
+        this.beginNanoSeconds = System.nanoTime();
     }
 
-    public void clear() {
-        this.beginTime = -1;
-        this.endTime = -1;
+    public void endNanoSeconds() {
+        this.endNanoSeconds = System.nanoTime();
     }
 
-    public void setBeginTime() {
-        this.beginTime = System.currentTimeMillis();
+    public long differenceNanoSeconds() {
+        return endNanoSeconds - beginNanoSeconds;
     }
 
-    public void setEndTime() {
-        this.endTime = System.currentTimeMillis();
+    public void beginMillisSeconds() {
+        this.beginMillisSeconds = System.currentTimeMillis();
+    }
+
+    public void endMillisSeconds() {
+        this.endMillisSeconds = System.currentTimeMillis();
+    }
+
+    public long differenceMillisSeconds() {
+        return endMillisSeconds - beginMillisSeconds;
+    }
+
+    public void beginLocalTime() {
+        this.beginLocalTime = LocalTime.now();
+    }
+
+    public void endLocalTime() {
+        this.endLocalTime = LocalTime.now();
+    }
+
+    public Duration differenceLocalTime() {
+        return Duration.between(beginLocalTime, endLocalTime);
     }
 
 }
